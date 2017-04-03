c42c496700345b634625c072e6d5e6a99b8523fc
nikoGao <niko_fan@sina.com>
2017-04-03 21:24:48 +0800

    Add a method to caculate postfix

--- Structure/Chapter_3/infixToPostfix.py
+++ Structure/Chapter_3/infixToPostfix.py
@@ -1,7 +1,7 @@
 from Structure.Chapter_3.Stack import Stack
 
 def infixToPostfix(forum):
-    prec = {'*':3, '/':3, '+':2, '-': 2, '(':1}
+    prec = {'*':3, '/':3, '+':2, '-': 2, '(':1, '^':4}
     opStack = Stack()
     oparend = []
     tokenlist = forum.split()
@@ -16,12 +16,36 @@ def infixToPostfix(forum):
                 oparend.append(topToken)
                 topToken = opStack.pop()
         else:
-            while not opStack.isEmpty() and prec[opStack.peek()] > prec[token]:
+            while not opStack.isEmpty() and prec[opStack.peek()] >= prec[token]:
                 oparend.append(opStack.pop())
             opStack.push(token)
     while not opStack.isEmpty():
         oparend.append(opStack.pop())
-    return ''.join(elem for elem in oparend)
+    return ''.join(oparend)
 
-print(infixToPostfix("A * B + C * D"))
-print(infixToPostfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+
+def postfixEval(postfixExpr):
+    operandStack = Stack()
+    tokenList = postfixExpr.split()
+    for token in tokenList:
+        if token.isdigit():
+            operandStack.push(token)
+        else:
+            operand2 = int(operandStack.pop())
+            operand1 = int(operandStack.pop())
+            result = doMath(operand1, operand2, token)
+            operandStack.push(result)
+    return operandStack.pop()
+
+
+def doMath(operand1, operand2, token):
+    if token == '+':
+        return operand1 + operand2
+    if token == '-':
+        return operand1 - operand2
+    if token == '*':
+        return operand1 * operand2
+    if token == '/':
+        return operand1 / operand2
+
+print(infixToPostfix("5 * 3 ^ ( 4 - 2 )"))
