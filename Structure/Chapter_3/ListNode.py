bebffaad266b7abcd7c6fea7b3587f854ef61bb0
nagao <nagao@redhat.com>
2017-04-06 17:13:45 +0800

    Add OrderedList

--- Structure/Chapter_3/ListNode.py
+++ Structure/Chapter_3/ListNode.py
@@ -95,10 +95,61 @@ class UnorderedList:
         return self.remove(self.getItem(index))
 
 
+class OrderedList:
+    def __init__(self):
+        self.head = None
 
+    def isEmpty(self):
+        return self.head == None
 
-mylist = UnorderedList()
+    def add(self, item):
+        previous = None
+        curr = self.head
+        while curr != None and curr.getData() < item:
+            previous = curr
+            curr = curr.getNext()
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            previous.setNext(temp)
+            temp.setNext(curr)
 
+    def size(self):
+        curr = self.head
+        count = 0
+        while curr != None:
+            count += 1
+            curr = curr.getNext()
+        return count
+
+    def search(self, target):
+        curr = self.head
+        while curr != None:
+            if curr.getData() == target:
+                return True
+            elif curr.getData() > target:
+                break
+            curr = curr.getNext()
+        return False
+
+    def remove(self, item):
+        prev, curr = None, self.head
+        found = False
+        while not found:
+            if curr.getData() == item:
+                found = True
+            else:
+                prev = curr
+                curr = curr.getNext()
+        "prev==None means that item is the first in the list"
+        if not prev:
+            self.head = curr.getNext()
+        else:
+            prev.setNext(curr.getNext())
+
+mylist = OrderedList()
 mylist.add(31)
 mylist.add(77)
 mylist.add(17)
@@ -109,15 +160,3 @@ mylist.add(54)
 print(mylist.size())
 print(mylist.search(93))
 print(mylist.search(100))
-
-mylist.add(100)
-print(mylist.search(100))
-print(mylist.size())
-
-mylist.remove(54)
-print(mylist.size())
-mylist.remove(93)
-print(mylist.size())
-mylist.remove(31)
-print(mylist.size())
-print(mylist.search(93))
